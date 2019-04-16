45751056955e90f4d0d51f414d39ad005d034810
jose <josmontenegro2012@gmail.com>
2019-04-16 19:22:06 -0400

    Rebuild

---
+++ dev/src/states/stateTextTool.js
@@ -0,0 +1,8 @@
+import GameState from "./gameState.js";
+
+export default class StateTextTool extends GameState{
+    constructor(){
+        super();
+        this.name = "StateTextTool";
+    }
+}
